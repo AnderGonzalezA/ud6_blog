d3de7ddd7109c64a56e0d7dc6b84397c73e45210
AnderGonzalezA <agonzalezam18dw@ikzubirimanteo.com>
2019-12-19 08:13:26 +0100

    5 en proceso

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,51 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
+  @foreach ($posts as $post)
+  @if (Auth::user()->id === $post->user->id)
+  <div class="row">
+    <!-- Post Content Column -->
+    <div class="col-lg-8">
+      <!-- Title -->
+      <h1 class="mt-4">{{$post->title}}</h1>
+      <!-- Author -->
+      <p class="lead">
+        by
+        <a href="#">{{$post->user->name}}</a>
+      </p>
+      <hr>
+      <!-- Date/Time -->
+      <p>{{$post->published_at}}</p>
+      <hr>
+      <!-- Preview Image -->
+      <img class="img-fluid rounded" src="http://placehold.it/900x300" alt="">
+      <hr>
+      <!-- Post Content -->
+      <p class="lead">{{$post->excerpt}}</p>
+      <p>{{$post->body}}</p>
+      <hr>
     </div>
+    <!-- Sidebar Widgets Column -->
+    <div class="col-md-4">
+      <!-- Categories Widget -->
+      <div class="card my-4">
+        <h5 class="card-header">Categories</h5>
+        <div class="card-body">
+          <div class="row">
+            <div class="col-lg-6">
+              <ul class="list-unstyled mb-0">
+                <li>
+                  <a href="#">{{$post->category->name}}</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- /.row -->
+  @endif
+  @endforeach
 </div>
 @endsection
