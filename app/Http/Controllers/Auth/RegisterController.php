cd3958860c4056df1c2b18cdc3da218d43828058
AnderGonzalezA <agonzalezam18dw@ikzubirimanteo.com>
2019-12-27 14:10:30 +0100

    register corregido

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -60,10 +60,13 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'role' => 'editor',
         ]);
+        $user->roles()->attach(Role::where('name', 'editor')->first());
+        return $user;
     }
 }
